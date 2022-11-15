121342cfbb6a3c4153211df8cf9afdc1efff4a5f
AndresCamiloChavez <andres.cz.chavez@gmail.com>
2022-11-15 16:05:40 -0500

    Diseño pantalla Permisos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maps_app/screens/screens.dart';
 
 void main() => runApp(const MapsApp());
 
@@ -7,17 +8,9 @@ class MapsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: GpsAccessScreen());
   }
 }
--- lib/screens/gps_access_screen.dart
+++ lib/screens/gps_access_screen.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
 
 class GpsAccessScreen extends StatelessWidget {
-   
   const GpsAccessScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-         child: Text('GpsAccessScreen'),
+        child: _AccessButtton(),
       ),
+    );
+  }
+}
+
+class _AccessButtton extends StatelessWidget {
+  const _AccessButtton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Es necesario acceso a GPS'),
+        MaterialButton(
+            child: const Text(
+              'Solicitar acceso',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.black,
+            shape: const StadiumBorder(),
+            elevation: 0,
+            splashColor: Colors.transparent,
+            onPressed: () {})
+      ],
+    );
+  }
+}
+
+class _EnableGPSMessage extends StatelessWidget {
+  const _EnableGPSMessage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Debe de habilitar el GPS',
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
     );
   }
 }
